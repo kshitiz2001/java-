320f2a610d233437fced23a844b5f4d22ee7c38e
kshitiz2001 <kshitiz.singh_cs20@gla.ac.in>
2024-07-09 17:38:17 +0530

    New

--- maxSubArray.java
+++ maxSubArray.java
@@ -1,6 +1,6 @@
 public class maxSubArray {
     public static void main(String[] args){
-        int[] arr = {1,2,-10,4,5};
+        int[] arr = {1,2,-1,4,5};
         int ans = Integer.MIN_VALUE;
         int sum = Integer.MIN_VALUE;
         for(int i=0;i<arr.length;i++){
