180ae20a16867e2f896eb8dd1945476f6efe52c3
kshitiz2001 <kshitiz.singh_cs20@gla.ac.in>
2024-07-16 17:08:21 +0530

    java

--- StaticDemo.java
+++ StaticDemo.java
@@ -1,6 +1,22 @@
-public class StaticDemo {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+class Demo{
+    static int b = 222;
+}
+public class StaticDemo {
+    static Scanner sc = new Scanner(System.in);
+    static int a = sc.nextInt();
+    public static void main(String[] args) {
+        System.out.println("by directly : "+a);
+        System.out.println("by className : "+StaticDemo.a);
+        StaticDemo sd = new StaticDemo();
+        System.out.println("by reference : "+sd.a);
+        System.out.println("by object : "+new StaticDemo().a);
+        System.out.println("---------------------------------");
+        System.out.println("by directly : "+Demo.b);
+        Demo d = new Demo();
+        System.out.println("by reference : "+d.b);
+        System.out.println("by object : "+new Demo().b);
 
     }
 }
